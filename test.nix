6ea598d0201ba8c3efcb15622e554d36a5c34da7
Dan Tkachov <danieltkachov67@gmail.com>
2024-12-24 17:42:30 -0500

    initial commit with hopefully working nix file

---
+++ test.nix
@@ -0,0 +1,41 @@
+{
+    description="Dans Nix Configuration for quickly installing all packages";
+
+    inputs = {
+        nixpkgs.url = "github:nixpkgs/nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = {nixpkgs, home-manager, ...}:
+    let
+        system = builtins.currentSystem;
+        pkgs = nixpkgs.legacyPackages.${system};
+    in{
+        homeConfigurations.dan = home-manager.lib.homeManagerConfiguration{
+            inherit pkgs;
+
+            modules = [
+                {
+                    home.packages = with pkgs; [
+                        tree
+                        cowsay
+
+                    ];
+
+                    home = {
+                        username = "dan";
+                        homeDirectory = "/home/dan";
+                        stateVersion = "24.11";
+
+                    };
+
+                    programs.home-manager.enable = true;
+                }
+            ]
+        }
+    };
+
+}
