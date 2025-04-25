3e71fc01c0f3ecb786c8f9d1f95f4fde4a3c99ad
Dan Tkachov <danieltkachov67@gmail.com>
2025-04-25 19:38:32 -0400

    added right nvtop

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
                         ventoy
                         neofetch
                         htop
-                        nvtop
+                        nvtopPackages.nvidia
 
                         # networking
                         wireshark
